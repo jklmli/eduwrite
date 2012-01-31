8d601b97cf76db84fae6e78b18c8b9676db093b7
cielo@ec2 <c.holic01@gmail.com>
2012-01-31 06:45:43 +0000

    port now can be specified by second argument

--- FrontEnd/MockServer.js
+++ FrontEnd/MockServer.js
@@ -1,7 +1,13 @@
 // Initialize the app
 var express = require('express');
 var app = express.createServer();
+var port;
 
+if(process.argv[2]==null){
+    port = 8000;
+} else {
+    port = process.argv[2];
+}
 // Attach URL handlers
 app.get('/', helloWorld);
 
@@ -12,5 +18,8 @@ function helloWorld(req, res) {
 
 
 
-// Start the app listening on port 8000
-app.listen(8000);
+// Start the app listening on default port 3000 or 
+// one from the econd rgument
+
+app.listen(port);
+console.log("Server listening on port %d in %s mode", app.address().port, app.settings.env);
