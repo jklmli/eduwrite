15f966e894f1c242ed3babca813363f6a924ec60
cielo@ec2 <c.holic01@gmail.com>
2012-02-11 22:34:15 +0000

    encrypt password

--- node/model/user.js
+++ node/model/user.js
@@ -1,12 +1,22 @@
 var client = require("../database.js").client;
 var Hashes = require('jshashes');
 var table = "users";
+var bcrypt = require('bcrypt');
 
-
+/**
+ *  Encrypt the password and return hash value
+ *  This is a sync method.
+ */
 var encrypt = function(password){
-    //TODO:
-    //  implement encryption mechanism for the password
-    return password;
+    var salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password,salt);
+}
+
+/**
+ *  Compare password with hash from db.
+ */
+var comparePassword = function(password,hash){
+    return bcrypt.compare(password, hash);
 }
 
 exports.get = function(id,cb){
@@ -18,9 +28,11 @@ exports.get_by_email = function(email,cb){
 }
 
 exports.get_by_email_and_password = function(email,password,cb){
-    client.get('users').where("email='"+email+"' and password='"+encrypt(password)+"'").limit(1).execute(cb);
+
+    client.get('users').where("email='"+email+"' and password='"+comparePassword(password)+"'").limit(1).execute(cb);
 }
 
 exports.insert = function(user,cb){
+    user.password = encrypt(user.password);
     client.insert('users',user,cb);
 }
