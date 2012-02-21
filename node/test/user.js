71f0c38cb94be02d53500661d8a806bcd95ff444
cielo <c.holic@gmail.com>
2012-02-21 01:00:26 +0000

    made test case for user.js

--- node/test/user.js
+++ node/test/user.js
@@ -1,8 +1,14 @@
 var assert = require("assert");
+var user = require("../model/user.js");
 
-var test = function(){
     //run tests
-    assert.equal(1,1);
-    assert.equal(2,1);
-}
-exports.test = test;
+var testGet = user.get(1,function(e){
+    assert.equal(e.length,0);
+});
+
+
+var testGetEmail = user.get_by_email("kduber2@illinois.edu",function(e){
+    console.log(e);
+    assert.equal(e.length,1);
+    assert.equal(e[0].email,"kduber2@illinois.edu");
+});
