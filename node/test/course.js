3a018b1e9fa27d239e3662d7f4555df560f8287d
Kevin Duber <kduber2@illinois.edu>
2012-02-20 20:27:31 -0600

    beginnings of course test cases

--- node/test/course.js
+++ node/test/course.js
@@ -1,7 +1,27 @@
 var assert = require("assert");
+var course = require("../model/user.js")
 
-var test = function(){
+var count = 0;
 
-}
+var testFailGetByID = course.get(-1, function(e){
+	try {
+		assert.equal(0, e.length)
+		console.log("Passed invalid id test")
+	}
+	catch(err) {
+		console.log(err);
+	}
+});
+
+var testlGetByID = course.get(2, function(e){
+	try {
+		assert.equal(1, e.length)
+		//TODO: confirm entry matches one in DB
+		console.log("Passed real id test")
+	}
+	catch(err) {
+		console.log(err);
+	}
+});
 
 exports.test = test;
