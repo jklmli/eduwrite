3aa7b123ff7639b6eca4a16e91d73f83d1a0cbe3
cielo <c.holic@gmail.com>
2012-02-29 20:02:56 +0000

    added get_by_lecture & get_by_lecture id methods in note model

--- node/model/note.js
+++ node/model/note.js
@@ -7,11 +7,28 @@ exports.createGroup = function(){
     });
 };
 
-
 exports.get_by_user = function(user,cb){
     get_by_user_id(user.id,user.name,cb);
 }
 
 exports.get_by_user_id = get_by_user_id = function(user_id,user_name,cb){
-    api.createAuthorIfNotExistsFor(user_id,"user_name",cb);
+    //get list of sessions from author
+    api.createAuthorIfNotExistsFor(user_id,"default_name",function(err,author){
+        if(err)
+            throw err;
+        cb(author);   
+    });
 }
+
+exports.get_by_lecture = function(lecture,cb){
+    get_by_lecture_id(lecture.id,cb);
+}
+
+exports.get_by_lecture_id = get_by_lecture_id = function(lecture_id,cb){
+    api.createGroupIfNotExistsFor(lecture_id,function(err,group){
+        if(err)
+            throw err;
+        cb(group);
+    });
+}
+
