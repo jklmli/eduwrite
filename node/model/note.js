141cb71d84e7f2c9298ff918156bd68c18cdbc80
cielo <c.holic@gmail.com>
2012-02-29 20:49:46 +0000

    create/destroy pads that does not belong to lecture(group)

--- node/model/note.js
+++ node/model/note.js
@@ -2,12 +2,17 @@ var client = require("./database.js");
 var user = require('./user.js');
 var api = require("../db/API.js");
 
-exports.createGroup = function(){
-        api.createGroupIfNotExistsFor("-1241",function(e){
+/*
+ *  Create group ID
+ */
+exports.createGroup = function(groupID){
+        api.createGroupIfNotExistsFor(groupID,function(e){
         console.log(e);
     });
 };
-
+/*
+ * Get pads that belongs to the user.
+ */
 exports.get_by_user = function(user,cb){
     if(user.aid==null){
         api.createAuthorIfNotExistsFor(user.id,user.name,function(err,response){
@@ -22,17 +27,25 @@ exports.get_by_user = function(user,cb){
     }
 }
 
+/*
+ * Get pads that belong to the author_id of the user
+ */
 exports.get_by_author_id = get_by_author_id = function(aid,name,cb){
-    //get sessions by user
-    //
-    
-
+    //TODO:get sessions by user
+    //get groups that the author belongs to 
+    //get pads for each group
 }
 
+/*
+ * Get pads that belongs to the lecture(group)
+ */
 exports.get_by_lecture = function(lecture,cb){
     get_by_lecture_id(lecture.id,cb);
 }
 
+/*
+ * Get pads that belongs to the lecture_id(group_id)
+ */
 exports.get_by_lecture_id = get_by_lecture_id = function(lecture_id,cb){
     api.createGroupIfNotExistsFor(lecture_id,function(err,group){
         if(err)
@@ -40,11 +53,29 @@ exports.get_by_lecture_id = get_by_lecture_id = function(lecture_id,cb){
         console.log(group);
     });
 }
-
+/*
+ * Set password for the pad with padID
+ */
 exports.setPassword = function(padID, password,cb){
     api.setPassword(padID,password,function(err,response){
         if(err)
             throw err;
         cb(response);
     })   
+}
+
+/*
+ * Create pad that does not belong to any group
+ */
+exports.create = function(padID, cb){
+    api.createPad(padID, "", cb);
+}
+
+/*
+ * Destroy pad with specific ID
+ */
+exports.destroy = function(padID, cb){
+    api.deletePad(padID, function(err,response){
+        cb(response);
+    });
 }
