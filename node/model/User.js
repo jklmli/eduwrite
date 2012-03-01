e60c4515cda1a6ad2f467a693ec646adb7e6fc18
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-02-29 22:55:12 -0600

    Added documentation to User.js & improve some naming

--- node/model/User.js
+++ node/model/User.js
@@ -3,62 +3,93 @@ var Hashes = require('jshashes');
 var table = "users";
 var bcrypt = require('bcrypt');
 
+
 /**
- *  Encrypt the password and return hash value
- *  This is a sync method.
+ *  Encrypt the password and return hash value. This is a synchronous method.
  */
 var encrypt = function (password) {
   //we are using Sync method
   var salt = bcrypt.genSaltSync(10);
   return bcrypt.hashSync(password, salt);
-}
+};
+
 
 /**
  *  Compare password with hash from db.
  */
 var comparePassword = function (password, hash) {
   return bcrypt.compareSync(password, hash);
-}
-
-exports.get = function (id, cb) {
-  client.get(table).where("id='" + id + "'").limit(1).execute(cb);
-}
-
-exports.get_by_email = function (email, cb) {
-  client.get(table).where("email='" + email + "'").limit(1).execute(cb);
-}
-
-exports.destroy = function (id, cb) {
-  client.destroy(table).where("id='" + id + "'").execute(cb);
-}
+};
 
 
-exports.get_by_email_and_password = function (email, password, cb) {
+/**
+ * Gets an element from a table in the database given its id
+ *  @param id        The id of the entry to grab
+ *  @param callback  The callback to perform on success
+ */
+exports.get = function (id, callback) {
+  client.get(table).where("id='" + id + "'").limit(1).execute(callback);
+};
 
-  client.get(table).where("email='" + email + "'").limit(1).execute(function (e) {
-    if (e.length < 1) {
-      cb(e);
+
+/**
+ * Gets an element from a table in the database by its email
+ *  @param email     The id of the entry to grab
+ *  @param callback  The callback to perform on success
+ */
+exports.get_by_email = function (email, callback) {
+  client.get(table).where("email='" + email + "'").limit(1).execute(callback);
+};
+
+
+/**
+ * Removes an element from a table in the database given its id
+ *  @param id        The id of the entry to grab
+ *  @param callback  The callback to perform on success
+ */
+exports.destroy = function (id, callback) {
+  client.destroy(table).where("id='" + id + "'").execute(callback);
+};
+
+
+/**
+ * Gets an element from a table in the database by its hashed email and password
+ *  @param callback  The callback to perform on success
+ */
+exports.get_by_email_and_password = function (email, password, callback) {
+
+  client.get(table).where("email='" + email + "'").limit(1).execute(function (data) {
+    if (data.length < 1) {
+      callback(data);
     } else {
-      var user = e[0];
+      var user = data[0];
       var pwd = user.password;
       if (comparePassword(password, pwd)) {
-        cb(e);
+        callback(data);
       } else {
-        cb([]);
+        callback([]);
       }
     }
   });
+};
 
-}
 
-exports.insert = function (user, cb) {
-  user.password = encrypt(user.password);
-  client.insert(table, user, cb);
-}
-
-/*
- *  Update a user's stored info in the database
+/**
+ * Inserts a user into the database
+ *  @param user      The user data
+ *  @param callback  The callback to perform on success
  */
-exports.update = function (user, cb) {
-  client.update(table, user, cb).where("id = " + user);
-}
+exports.insert = function (user, callback) {
+  user.password = encrypt(user.password);
+  client.insert(table, user, callback);
+};
+
+
+/**
+ * Updates a user into the database
+ *  @param user      The user
+ *  @param callback  The callback to perform on success
+ */
+exports.update = function (user, callback) {
+  client.update(table, user, callback).where("id = " + user);
+};
