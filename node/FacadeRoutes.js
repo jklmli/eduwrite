2798a5b96e638f5bfe08eb18564a0c5b38565b3b
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-02-29 20:48:00 -0600

    Documented FacadeRoutes

--- node/FacadeRoutes.js
+++ node/FacadeRoutes.js
@@ -4,29 +4,35 @@ var liveFacade = require('./facade/live/liveFacade.js');
 
 var facade = mockFacade;
 
-// Attach URL handlers for facade calls
+// User registration functions
 exports.getUser = facade.getUser;
 exports.closeAccount = facade.closeAccount;
 exports.register = facade.register;
 
+// User permissions functions
 exports.setPermissions = facade.setPermissions;
 
+// User notes functions
 exports.getNotes = facade.getNotes;
 exports.addNote = facade.addNote;
 exports.removeNote = facade.removeNote;
 exports.getNotesByUserId = facade.getNotesByUserId;
 exports.updateNote = facade.updateNote;
 
+// User lectures functions
 exports.getNotesByLectureId = facade.getNotesByLectureId;
 
+// User classes functions
 exports.getLecturesByClassId = facade.getLecturesByClassId;
 exports.getClasses = facade.getClasses;
 exports.getInstructor = facade.getInstructor;
 
+// Instructor functions
 exports.addLecture = facade.addLecture;
 exports.addClass = facade.addClass;
 exports.removeUser = facade.removeUser;
 
+// User + instructor account functionality
 exports.login = facade.login;
 exports.logout = facade.logout;
 exports.isLoggedIn = facade.isLoggedIn;
