1c5e66beed375ed673dc94e6d212026b999ea05f
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-02-29 22:59:03 -0600

    Added documentation to Course.js & Note.js

--- node/model/Note.js
+++ node/model/Note.js
@@ -2,7 +2,8 @@ var client = require("./Database.js");
 var user = require('./User.js');
 var api = require("../db/API.js");
 
-/*
+
+/**
  *  Create group ID
  */
 exports.createGroup = function (groupID) {
@@ -10,50 +11,57 @@ exports.createGroup = function (groupID) {
     console.log(e);
   });
 };
-/*
+
+
+/**
  * Get pads that belongs to the user.
  */
-exports.get_by_user = function (user, cb) {
+exports.get_by_user = function (user, callback) {
   if (user.aid == null) {
     api.createAuthorIfNotExistsFor(user.id, user.name, function (err, response) {
       if (err)
         throw err;
       aid = response.authorID;
       //TODO: update user info, and put authorID
-      get_by_author_id(aid, cb);
+      get_by_author_id(aid, callback);
     });
   } else {
-    get_by_author_id(user.aid, cb);
+    get_by_author_id(user.aid, callback);
   }
-}
+};
 
-/*
+
+/**
  * Get pads that belong to the author_id of the user
  */
-exports.get_by_author_id = get_by_author_id = function (aid, name, cb) {
+exports.get_by_author_id = get_by_author_id = function (aid, name, callback) {
   //TODO:get sessions by user
   //get groups that the author belongs to
   //get pads for each group
-}
+};
 
-/*
+
+/**
  * Get pads that belongs to the lecture(group)
  */
-exports.get_by_lecture = function (lecture, cb) {
-  get_by_lecture_id(lecture.id, cb);
-}
+exports.get_by_lecture = function (lecture, callback) {
+  get_by_lecture_id(lecture.id, callback);
+};
 
-/*
+
+/**
  * Get pads that belongs to the lecture_id(group_id)
  */
-exports.get_by_lecture_id = get_by_lecture_id = function (lecture_id, cb) {
-  api.createGroupIfNotExistsFor(lecture_id, function (err, group) {
-    if (err)
-      throw err;
+exports.get_by_lecture_id = get_by_lecture_id = function (lecture_id, callback) {
+  api.createGroupIfNotExistsFor(lecture_id, function (error, group) {
+    if (error)
+      throw error;
     console.log(group);
   });
-}
-/*
+};
+
+
+/**
  * Set password for the pad with padID
  */
 exports.setPassword = function (padID, password, cb) {
@@ -62,20 +70,22 @@ exports.setPassword = function (padID, password, cb) {
       throw err;
     cb(response);
   })
-}
+};
 
-/*
+
+/**
  * Create pad that does not belong to any group
  */
-exports.create = function (padID, cb) {
-  api.createPad(padID, "", cb);
-}
+exports.create = function (padID, callback) {
+  api.createPad(padID, "", callback);
+};
 
-/*
+
+/**
  * Destroy pad with specific ID
  */
-exports.destroy = function (padID, cb) {
+exports.destroy = function (padID, callback) {
   api.deletePad(padID, function (err, response) {
-    cb(response);
+    callback(response);
   });
-}
+};
