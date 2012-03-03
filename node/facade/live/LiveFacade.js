3a3873b29c4cd41a248bb3eded76613b2772660d
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-03-03 13:36:51 -0600

    Properly separated login into Authentication model & fixed bugs

--- node/facade/live/LiveFacade.js
+++ node/facade/live/LiveFacade.js
@@ -1,7 +1,6 @@
 var User = require('../../model/User.js');
 var Note = require('../../model/Note.js');
-var login = require('../../model/Login.js');
-var register = require('../../model/Register.js');
+var Authentication = require('../../model/Authentication.js');
 
 
 /**
@@ -13,37 +12,63 @@ exports.register = function (req, res) {
   var password = req.body.password;
 
   // Get the user by email address
-  User.getByEmail(email, login.login);
-};
+  User.getByEmail(email, function (usersFound) {
 
+    if(Authentication.register(usersFound, req)) {
 
-/**
- * Login a user if they exist and submit correct credentials, fail otherwise.
- */
-exports.login = function (req, res) {
+      req.flash("success", "You have been successfully registered to the site");
+      res.redirect('/');
 
-  var email = req.body.email;
-  var password = req.body.password;
+    } else {
 
-  User.getByEmailAndPassword(email, password, register.register);
-};
+      req.flash("error", "User with the email " + email + " already exists");
+      res.redirect('back');
 
-
-/**
- * Get the notes for some user
- */
-exports.getNotes = function (req, res) {
-  var user = req.session.user;
-  Note.getByUser(user, function (err, notes) {
-    console.log(notes);
+    }
   });
 };
 
+  /**
+   * Login a user if they exist and submit correct credentials, fail otherwise.
+   */
+  exports.login = function (req, res) {
 
-/**
- * Add a new note for some user
- */
-exports.addNote = function (req, res) {
-};
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.getByEmailAndPassword(email, password, function(usersFound) {
+
+      // Display login success or failure message based on whether or not the login succeeded
+      if(Authentication.login(usersFound, req)) {
+
+        req.flash("success", "You have been successfully logged in to the site");
+        res.redirect('/');
+
+      } else {
+
+        req.flash("error", "You have entered incorrect password, or the user with the email does not exists.");
+        res.redirect('back');
+
+      }
+    });
+  };
+
+
+  /**
+   * Get the notes for some user
+   */
+  exports.getNotes = function (req, res) {
+    var user = req.session.user;
+    Note.getByUser(user, function (err, notes) {
+      console.log(notes);
+    });
+  };
+
+
+  /**
+   * Add a new note for some user
+   */
+  exports.addNote = function (req, res) {
+  };
 
 
