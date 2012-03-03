d1d192ca43a8b07a39b5c7afcd25fef40cd86b0e
Jon Tedesco <jon.c.tedesco@gmail.com>
2012-03-03 13:09:39 -0600

    Added TestDatabase file and separated login & register tests into separate files

--- test/model/LoginTest.js
+++ test/model/LoginTest.js
@@ -1,19 +1,4 @@
-/*
- * Test cases for registration and login functions
- */
-
-var facade = require("../../node/facade/live/LiveFacade.js");
-
-
-describe("register", function () {
-  it("should fail if email field is empty");
-
-  it("should fail if password field is empty");
-
-  it("should fail if email already exists");
-
-  it("should succeed if email, password are given and email doesn't exist");
-});
+var login = require("../../node/model/Login.js");
 
 
 describe("login", function () {
