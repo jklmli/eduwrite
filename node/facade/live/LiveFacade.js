1a6c00f9fb8abc90dd4c6e9bbfcb5f3e64030b3e
cielo <c.holic@gmail.com>
2012-03-03 19:46:43 +0000

    added debugging to getNodetesByUserId

--- node/facade/live/LiveFacade.js
+++ node/facade/live/LiveFacade.js
@@ -14,17 +14,55 @@ exports.register = function (req, res) {
   // Get the user by email address
   User.getByEmail(email, function (usersFound) {
 
-    if(Authentication.register(usersFound, req)) {
+      if(Authentication.register(usersFound, req)) {
 
       req.flash("success", "You have been successfully registered to the site");
       res.redirect('/');
 
-    } else {
+      } else {
 
       req.flash("error", "User with the email " + email + " already exists");
       res.redirect('back');
+      }
+      });
+};
 
-<<<<<<< HEAD
+/**
+ * Login a user if they exist and submit correct credentials, fail otherwise.
+ */
+exports.login = function (req, res) {
+
+  var email = req.body.email;
+  var password = req.body.password;
+
+  User.getByEmailAndPassword(email, password, function(usersFound) {
+
+      // Display login success or failure message based on whether or not the login succeeded
+      if(Authentication.login(usersFound, req)) {
+
+      req.flash("success", "You have been successfully logged in to the site");
+      res.redirect('/');
+
+      } else {
+
+      req.flash("error", "You have entered incorrect password, or the user with the email does not exists.");
+      res.redirect('back');
+
+      }
+      });
+};
+
+
+/**
+ * Get the notes for some user
+ */
+exports.getNotes = function (req, res) {
+  var user = req.session.user;
+  Note.getByUser(user, function (err, notes) {
+      console.log(notes);
+      });
+};
+
 
 /**
  * Get the notes for some user
@@ -38,33 +76,35 @@ exports.getNotesByUserId = function getNotesByUserId(req, res) {
       res.contentType('json');
       res.send(notes);
       console.log(notes);
-  });
+      console.log("1");
+      });
 };
+
 
 exports.getNotesByLectureId = function getNotesByLectureId(req, res) {
   var ret = [];
   var lectureId = req.body.lectureId;
   var notes = [
-    {
-      id:       9832498,
-      title:    "OH YEA",
-      lectureId:8234
-    },
-    {
-      id:       3429873,
-      title:    "My note",
-      lectureId:8234
-    },
-    {
-      id:       328932,
-      title:    "WOAH",
-      lectureId:8236
-    },
-    {
-      id:       847592,
-      title:    "BOOYAH",
-      lectureId:8236
-    }
+  {
+id:       9832498,
+            title:    "OH YEA",
+            lectureId:8234
+  },
+  {
+id:       3429873,
+          title:    "My note",
+          lectureId:8234
+  },
+  {
+id:       328932,
+          title:    "WOAH",
+          lectureId:8236
+  },
+  {
+id:       847592,
+          title:    "BOOYAH",
+          lectureId:8236
+  }
   ];
   for (i in notes) {
     if (notes[i].lectureId == lectureId)
@@ -79,60 +119,11 @@ exports.getNotesByLectureId = function getNotesByLectureId(req, res) {
  * Add a new note for some user
  */
 exports.addNote = function (req, res) {
-    var user = req.session.user;
-    if(!user){
-        res.send("Please login first");
-    }
-    Note.create(user.id,1,null);
-    res.send("Hello world");
+  var user = req.session.user;
+  if(!user){
+    res.send("Please login first");
+  }
+  Note.create(user.id,1,null);
+  res.send("Hello world");
 };
-=======
-    }
-  });
-};
-
-  /**
-   * Login a user if they exist and submit correct credentials, fail otherwise.
-   */
-  exports.login = function (req, res) {
-
-    var email = req.body.email;
-    var password = req.body.password;
-
-    User.getByEmailAndPassword(email, password, function(usersFound) {
-
-      // Display login success or failure message based on whether or not the login succeeded
-      if(Authentication.login(usersFound, req)) {
-
-        req.flash("success", "You have been successfully logged in to the site");
-        res.redirect('/');
-
-      } else {
-
-        req.flash("error", "You have entered incorrect password, or the user with the email does not exists.");
-        res.redirect('back');
-
-      }
-    });
-  };
-
-
-  /**
-   * Get the notes for some user
-   */
-  exports.getNotes = function (req, res) {
-    var user = req.session.user;
-    Note.getByUser(user, function (err, notes) {
-      console.log(notes);
-    });
-  };
-
-
-  /**
-   * Add a new note for some user
-   */
-  exports.addNote = function (req, res) {
-  };
->>>>>>> d741819ea19a45593f0be490e2703dfffed00a07
-
 
