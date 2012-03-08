7f081b57035cb139e7a678fd8ae76cb2a92096be
Will Parkin <wmparkin@gmail.com>
2012-03-07 20:39:07 -0600

    began filling in accountManagement pages

--- node/routes/FacadeRoutes.js
+++ node/routes/FacadeRoutes.js
@@ -2,7 +2,7 @@
 var mockFacade = require('../facade/mock/MockFacade.js');
 var liveFacade = require('../facade/live/LiveFacade.js');
 
-var facade = liveFacade;
+var facade = mockFacade;
 
 // User registration functions
 exports.getUser = facade.getUser;
