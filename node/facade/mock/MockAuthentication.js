382ad9a8dce754081afebbf593cf99d55ec52a1d
Jiawei Li <jiawei.h.li@gmail.com>
2012-03-24 02:17:58 -0500

    refactoring node/facade/mock/MockAuthentication.js

--- node/facade/mock/MockAuthentication.js
+++ node/facade/mock/MockAuthentication.js
@@ -4,71 +4,71 @@
 
 var loggedIn = {};
 
-/**
- * Register a new user account (just logs them in)
- */
-exports.register = function register(req, res) {
-  exports.login(req, res);
-};
+module.exports = {
+  /**
+   * Register a new user account (just logs them in)
+   */
+  register:function register(req, res) {
+    this.login(req, res);
+  },
 
+  /**
+   * Logs the user in without authentication
+   */
+  login:function login(req, res) {
 
-/**
- * Logs the user in without authentication
- */
-exports.login = function login(req, res) {
+    if (!this.isLoggedIn(req.session)) {
 
-  if (!exports.isLoggedIn(req.session)) {
+      var email = req.param('email');
+      var password = req.param('password');
 
-    var email = req.param('email');
-    var password = req.param('password');
+      // Store login in session
+      req.session.user = {
+        email:email,
+        password:password
+      };
 
-    // Store login in session
-    req.session.user = {
-      email:   email,
-      password:password
-    };
+      if (email && password) {
 
-    if (email && password) {
+        loggedIn[email] = true;
+        req.flash("success", "Successfully logged in with " + email);
+        console.log('Logged in as ' + email + '!');
 
-      loggedIn[email] = true;
-      req.flash("success", "Successfully logged in with " + email);
-      console.log('Logged in as ' + email + '!');
+      } else {
+        console.log('Error, missing email or password for login!');
+      }
+    } else {
+      console.log('Error, already logged in as ' + req.session.user.email + '!');
+    }
+    res.redirect('/');
+  },
+
+  /**
+   * Log out of the user's current session
+   */
+  logout:function (req, res) {
+
+    if (this.isLoggedIn(req.session)) {
+
+      var email = req.session.user.email;
+      loggedIn[email] = false;
+
+      console.log('Logged out of ' + email);
+
+      req.session.user = null;
 
     } else {
-      console.log('Error, missing email or password for login!');
+      res.redirect('/');
     }
-  } else {
-    console.log('Error, already logged in as ' + req.session.user.email + '!');
+  },
+
+  /**
+   * Checks if a user is logged in. (Silly implementation)
+   *  @param session  The session from the request
+   */
+  isLoggedIn:function (session) {
+    if (session && session.user)
+      console.log("Checking logged in..." + loggedIn[session.user.email]);
+    return session && session.user && loggedIn[session.user.email];
   }
-  res.redirect('/');
-};
-
-
-/**
- * Log out of the user's current session
- */
-module.exports.logout = function logout(req, res) {
-
-  if (exports.isLoggedIn(req.session)) {
-
-    var email = req.session.user.email;
-    loggedIn[email] = false;
-
-    console.log('Logged out of ' + email);
-
-    req.session.user = null;
-
-  } else {
-    res.redirect('/');
-  }
-};
-
-/**
- * Checks if a user is logged in. (Silly implementation)
- *  @param session  The session from the request
- */
-module.exports.isLoggedIn = function isLoggedIn(session) {
-  if (session && session.user)
-    console.log("Checking logged in..." + loggedIn[session.user.email]);
-  return session && session.user && loggedIn[session.user.email];
 };
